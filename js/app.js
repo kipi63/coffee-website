7855eed16723a324c3ea38562e2693b20d8c6552
Didar <didarb99@gmail.com>
2022-07-30 22:30:19 +0300

    night-30-07

--- js/app.js
+++ js/app.js
@@ -1,5 +1,50 @@
 let navbar = document.querySelector('.header__nav');
+let cartItem = document.querySelector('.cart');
+let searchForm = document.querySelector('.header__search');
 
 document.querySelector('#menu-btn').onclick = () => {
 	navbar.classList.toggle('active');
+	removeCartItem ();
+	removeSearchForm ();
+
+	
+}
+
+document.querySelector('#cart-btn').onclick = () => {
+	cartItem.classList.toggle('active');
+	removeNavbar ();
+	removeSearchForm ();
+}
+
+document.querySelector('#search-btn').onclick = () => {
+	searchForm.classList.toggle('active');
+	removeNavbar ();
+	removeCartItem ();
+}
+
+window.onscroll = () => {
+	navbar.classList.remove('active');
+	cartItem.classList.remove('active');
+	searchForm.classList.remove('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+function removeNavbar () {
+	navbar.classList.remove('active');
+}
+function removeCartItem () {
+	cartItem.classList.remove('active');
+}
+ 
+function removeSearchForm () {
+	searchForm.classList.remove('active');
 }
